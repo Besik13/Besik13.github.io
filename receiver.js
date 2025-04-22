5da1cb647e6dea244ed2eeedaf33a7cbe5bb717b
Yehor <egor.besik@gmail.com>
2025-04-23 00:01:07 +0300

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -2,6 +2,10 @@ const context = cast.framework.CastReceiverContext.getInstance();
 
 const playerManager = context.getPlayerManager();
 
+// Debug Logger
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+const LOG_TAG = 'MyAPP.LOG';
+
 // Enable HLS playback
 playerManager.setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD,
@@ -13,5 +17,23 @@ playerManager.setMessageInterceptor(
     return loadRequestData;
   }
 );
+
+// Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+context.addEventListener(cast.framework.system.EventType.READY, () => {
+  if (!castDebugLogger.debugOverlayElement_) {
+      castDebugLogger.setEnabled(true);
+  }
+});
+
+// Set verbosity level for Core events.
+castDebugLogger.loggerLevelByEvents = {
+  'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
+  'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+};
+
+// Set verbosity level for custom tags.
+castDebugLogger.loggerLevelByTags = {
+    LOG_TAG: cast.framework.LoggerLevel.DEBUG, // display all levels
+};
 
 context.start();
