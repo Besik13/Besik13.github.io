8dd4ad9774f7da1f3cfcfe004a3321a6c55a2be2
Yehor <egor.besik@gmail.com>
2025-04-22 23:15:51 +0300

    Create receiver.js

---
+++ receiver.js
@@ -0,0 +1,25 @@
+const context = cast.framework.CastReceiverContext.getInstance();
+
+const playerManager = context.getPlayerManager();
+
+// Enable HLS playback
+playerManager.setMessageInterceptor(
+  cast.framework.messages.MessageType.LOAD,
+  loadRequestData => {
+    console.log('Received LOAD request');
+    if (loadRequestData.media.contentType === 'application/x-mpegURL') {
+      console.log('HLS stream detected');
+    }
+    return loadRequestData;
+  }
+);
+
+// Optional: add event logging
+playerManager.addEventListener(
+  cast.framework.events.EventType.PLAYER_STATE_CHANGED,
+  event => {
+    console.log(`Player State Changed: ${event.playerState}`);
+  }
+);
+
+context.start();
